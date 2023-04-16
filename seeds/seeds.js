7bb17340eeebd16779395534e76d721150c0dd85
Victoria Benoit <vrose834@gmail.com>
2023-04-16 14:37:03 -0300

    Finished seeds

---
+++ seeds/seeds.js
@@ -0,0 +1,31 @@
+const sequelize = require('../config/connection');
+const { Traveller, Trip, Location } = require('../models');
+
+const travellerData = require('./travellersData.json');
+const locationData = require('./locationData.json');
+
+const seedDatabase = async() => {
+    try{
+    await sequelize.sync({force: true});
+    const travellers = await Traveller.bulkCreate(travellerData);
+    const locations = await Location.bulkCreate(locationData);
+
+    for(let i = 0; i < 10; i++){
+        const {id: travellerId} = travellers [
+            Math.floor(Math.random() * travellers.length)
+        ];
+        const {id: locationId} = locations[
+            Math.floor(Math.random() * locations.length)
+        ];
+        await Trip.create({
+            trip_budget: Math.random() * 10000 + 100,
+            traveller_amonut: Math.floor(Math.random()*10)+1,
+            traveller_id: travellerId,
+            location_id: locationId
+        })
+    }} catch(err){
+        console.log(err);
+    }
+    process.exit(0);
+}
+seedDatabase();
