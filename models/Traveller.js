8283923bebe355bc88d474d3c441e2f3212d13bc
Victoria Benoit <vrose834@gmail.com>
2023-04-16 13:54:45 -0300

    Finished the traveller model

---
+++ models/Traveller.js
@@ -0,0 +1,34 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Traveller extends Model{};
+
+Traveller.init(
+    {
+        id:{
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        }
+    },{
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'traveller'
+    }
+)
+module.exports = Traveller;
